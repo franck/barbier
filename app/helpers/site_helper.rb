e6dbe67acea4a7cb636af694e783c97b73cdf308
Franck D'agostini <franck.dagostini@gmail.com>
2012-02-21 16:30:22 +0100

    small refactoring

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -3,12 +3,14 @@ module SiteHelper
   
   def site_nav
     items = []
+    items << content_tag('li', "Basic", :class => 'nav-header')
     items << content_tag('li', link_to('Messages', prive_site_messages_path), :class => option_nav_item_active?('messages'))
     items << content_tag('li', link_to('Informations', prive_site_informations_path), :class => site_nav_item_active?('informations'))
     items << content_tag('li', link_to('Photos', prive_site_photos_path), :class => option_nav_item_active?('photos'))
     items << content_tag('li', link_to('Tarifs', prive_site_tarifs_path), :class => option_nav_item_active?('tarifs'))
+    items << content_tag('li', "Avancée", :class => 'nav-header')
     items << content_tag('li', link_to('Thèmes', prive_site_themes_path), :class => option_nav_item_active?('themes'))
-    content_tag('ul', items.join.html_safe, :class => 'nav nav-pills nav-stacked')
+    content_tag('ul', items.join.html_safe, :class => 'nav nav-list')
   end
   
   def site_nav_item_active?(current_controller)
